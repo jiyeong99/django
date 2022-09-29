864d4fec5af6dbb0d3cdbac4df3cc7a48ee5f023
jiyeong99 <1999wldud@gmail.com>
2022-09-30 02:51:13 +0900

    .

--- Todo/Todo/posts/urls.py
+++ Todo/Todo/posts/urls.py
@@ -5,6 +5,13 @@ app_name = 'posts'
 
 urlpatterns = [
   path('', views.index, name='index'),
+
   path('new/', views.new, name='new'),
   path('create/', views.create, name='create'),
+
+  path('detail/<int:pk_>', views.detail, name='detail'),
+  path('edit/<int:pk_>', views.edit, name='edit'),
+  path('update/<int:pk_>', views.update, name='update'),
+
+  path('delete/<int:pk>', views.delete, name='delete'),
 ]
--- Todo/Todo/posts/views.py
+++ Todo/Todo/posts/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from .models import Post
 
@@ -29,9 +30,48 @@ def create(request):
         'created_at' : created_at_,
         'deadline' : deadline_,
     }
-    return render(request, 'posts/create.html', context)
+    # return render(request, 'posts/create.html', context)
+    return redirect('posts:index')
 
 def new(request):
     return render(request, 'posts/new.html')
-# def create(request):
-#     return render(request, 'posts/create.html')
+
+def edit(request, pk_):
+    post = Post.objects.get(pk = pk_)
+    context = {
+        "post" : post,
+    }
+    return render(request, 'posts/edit.html', context)
+
+def delete(request, pk) :
+    Post.objects.get(id=pk).delete()
+    return redirect('posts:index')
+
+def detail(request, pk_):
+    post = Post.objects.get(pk = pk_)
+    context = {
+        'post' : post,
+    }
+    return render(request, 'posts/detail.html', context)
+
+def update(request, pk_):
+    post = Post.objects.get(pk = pk_)
+
+    u_content = request.GET.get('content')
+    u_completed = request.GET.get('completed')
+    u_priority = request.GET.get('priority')
+    u_created_at = request.GET.get('created_at')
+    u_deadline = request.GET.get('deadline')
+
+    post.content = u_content
+    post.save()
+    post.completed = u_completed
+    post.save()
+    post.priority = u_priority
+    post.save()
+    post.created_at = u_created_at
+    post.save()
+    post.deadline = u_deadline
+    post.save()
+    
+    return redirect('posts:detail', post.pk)
