41d1500dc6d24dba29a7e54bfd61e42d853053df
jiyeong99 <1999wldud@gmail.com>
2022-10-04 17:52:44 +0900

    20221004

---
+++ postpost/popo/articles/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import redirect, render
+from .models import Article
+from .forms import Article, ArticleForm
+from popo.settings import BASE_DIR
+
+# Create your views here.
+def index(request):
+    # 요청받은 정보를 pk의 내림차순으로 index.html로 전달
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        # post로 받은 데이터를 db에 저장하기
+        article_form = ArticleForm(request.POST)
+        # 유효성 검사
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:index')
+    else:
+        # get으로 받을 때?
+        article_form = ArticleForm()
+    context = {
+        'article_form' : article_form,
+    }
+    # new.html로 넘겨
+    return render(request, 'articles/new.html', context)
+
+def new(request):
+    article_form = ArticleForm()
+    context = {
+        'article_form' : article_form,
+    }
+    # 다하면 index.html로 넘겨
+    return render(request, 'articles/new.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('articles:detail', article.pk)
+    else :
+        article_form = ArticleForm(instance=article)
+    context = {
+        'article_form' : article_form,
+    }
+    return render(request, 'articles/update.html', context)
