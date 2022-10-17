c4326f8c212cb178e92cc4fe0da23c527c2a9e21
jiyeong99 <1999wldud@gmail.com>
2022-10-17 18:23:12 +0900

    create

--- 20221017/medi/articles/views.py
+++ 20221017/medi/articles/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Articles
+from .forms import Articles, ArticleForm
+from django.contrib import messages
+from medi.settings import BASE_DIR, STATIC_URL
 
 def index(request):
     articles = Articles.objects.all()
@@ -7,3 +10,17 @@ def index(request):
         'articles' : articles,
     }
     return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article_form.save()
+            messages.success(request, '글 작성이 완료되었습니다.')
+            return redirect('articles:index')
+    else: 
+        article_form = ArticleForm()
+    context = {
+        'article_form': article_form
+    }
+    return render(request, 'articles/create.html', context=context)
