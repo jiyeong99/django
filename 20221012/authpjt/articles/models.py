d56e67599394f2ca81ce4b23e53dcce51d723d7b
jiyeong99 <1999wldud@gmail.com>
2022-10-17 16:39:21 +0900

    settingmodelimage

--- 20221012/authpjt/articles/models.py
+++ 20221012/authpjt/articles/models.py
@@ -1,3 +1,5 @@
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 from django.db import models
 
 # Create your models here.
@@ -6,3 +8,7 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = ProcessedImageField(upload_to='images/', blank=True,
+                                processors=[ResizeToFill(1200, 960)],
+                                format='JPEG',
+                                options={'quality': 80})
