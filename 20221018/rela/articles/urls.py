5d8794058afe44ac2b9e8feba9d4c80b74991417
jiyeong99 <1999wldud@gmail.com>
2022-10-18 14:31:05 +0900

    mkdetail

--- 20221018/rela/articles/urls.py
+++ 20221018/rela/articles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'articles'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:article_pk>/', views.detail, name='detail'),
 ]
