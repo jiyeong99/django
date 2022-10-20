7bd703c8cc6e1436472a35e4a0daf8e46a1549a7
jiyeong99 <1999wldud@gmail.com>
2022-10-20 15:27:11 +0900

    accountfirstbecauseofrelativerelations

--- 20221020/mfcrud/articles/views.py
+++ 20221020/mfcrud/articles/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles':articles,
+    }
+    return render(request,'articles/index.html', context)
+
+def detail(request,article_pk):
+    return render(request,'articles/detail.html')
+
+def create(request):
+    
+    return render(request, 'articles/create.html')
+
+def update(request,article_pk):
+    return redirect('articles/detail.html', article_pk)
+
+def delete(request, article_pk):
+    return redirect('articles:index')
